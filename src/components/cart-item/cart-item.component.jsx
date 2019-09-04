3504c9c0e7223eb39bb4649d0bbe8c130fc3f518
Joe Romriell <joe@actop.com>
2019-09-04 09:52:02 -0700

    updating our application with reslect with memoized

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -6,8 +6,8 @@ const CartItem = ({ item: { imageUrl, price, name, quantity }}) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item'></img>
         <div className='item-details'>
-            <span calssName='name'>{name}</span>
-            <span calssName='price'>
+            <span className='name'>{name}</span>
+            <span className='price'>
                 {quantity} x ${price}
             </span>
         </div>
